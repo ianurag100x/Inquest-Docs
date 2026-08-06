98cc36198d3df0a4270bf5c6b90c2474d13a63ab
ianurag100x <anurag100x@gmail.com>
2026-08-06 05:46:22 +0530

    Auth loop bug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,14 +13,9 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     // Server-only values (never sent to the client bundle).
-    tornFactionId: process.env.TORN_FACTION_ID || "21665",
-    authSecret:
-      process.env.AUTH_SECRET ||
-      "f18a7ecee1b4584d9dcae77dbc28f72f21bafa9ac830f9409eee2726c0f16f08",
-    supabaseUrl:
-      process.env.SUPABASE_URL || "https://rwokbfpyxtzeqpntvhtm.supabase.co",
-    supabaseAnonKey:
-      process.env.SUPABASE_ANON_KEY ||
-      "sb_publishable_a6Z_3RnM_83a4uT9idsjfw_wVa2ImIr",
+    tornFactionId: process.env.TORN_FACTION_ID,
+    authSecret: process.env.AUTH_SECRET,
+    supabaseUrl: process.env.SUPABASE_URL,
+    supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
   },
 });
